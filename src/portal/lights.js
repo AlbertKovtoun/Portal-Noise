e4587014235c718e16a535803bf51014113c126d
AlbertKovtoun <kovtoun.albert@hotmail.com>
2021-09-22 11:15:20 +0200

    :sparkles: lights + srgbencoding

--- src/portal/lights.js
+++ src/portal/lights.js
@@ -3,9 +3,9 @@ import * as THREE from "three";
 import { scene } from "./scene";
 
 export const setLights = () => {
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
   scene.add(ambientLight);
 
-  const pointLight = new THREE.PointLight(0xffffff, 1);
+  const pointLight = new THREE.PointLight(0xffffff, 0.8);
   scene.add(pointLight);
 };
--- src/portal/room.js
+++ src/portal/room.js
@@ -12,8 +12,13 @@ const textureLoader = new TextureLoader();
 const marbleTexture = textureLoader.load("/assets/textures/marbleColor.jpg");
 
 //Define Materials
+const roofMaterial = new THREE.MeshStandardMaterial({ color: "white" });
 const wallMaterial = new THREE.MeshStandardMaterial({ color: "white" });
-const marbleMaterial = new THREE.MeshStandardMaterial({ map: marbleTexture });
+const marbleMaterial = new THREE.MeshStandardMaterial({
+  map: marbleTexture,
+  roughness: 0,
+});
+const lightMaterial = new THREE.MeshStandardMaterial({ emissive: 0xffffff });
 
 export const loadRoom = () => {
   gltfLoader.load("/assets/models/portalRoom.glb", (gltf) => {
@@ -39,8 +44,12 @@ export const loadRoom = () => {
       return child.name === "light2";
     });
 
+    //Assign Materials
+    roofMesh.material = roofMaterial;
     wallsMesh.material = wallMaterial;
     floorMesh.material = marbleMaterial;
+    light1Mesh.material = lightMaterial;
+    light2Mesh.material = lightMaterial;
 
     room.position.set(0, -1, 0);
     scene.add(room);
